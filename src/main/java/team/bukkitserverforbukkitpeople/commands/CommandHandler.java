682e93a920b42dd87f5f2246a42dba0b8ed337db
gomeow <davismariotti@gmail.com>
2013-01-23 16:22:46 -0800

    Convert stuff to maven

--- src/main/java/team/bukkitserverforbukkitpeople/commands/CommandHandler.java
+++ src/main/java/team/bukkitserverforbukkitpeople/commands/CommandHandler.java
@@ -3,6 +3,7 @@ package team.bukkitserverforbukkitpeople.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
 import team.bukkitserverforbukkitpeople.Main;
 
 public class CommandHandler implements CommandExecutor {
@@ -13,7 +14,6 @@ public class CommandHandler implements CommandExecutor {
 		this.plugin = main;
 	}
 	
-	@Override
 	public boolean onCommand(CommandSender cs, Command cmdObj, String label, String[] args) {
 		String cmd = cmdObj.getName();
 		if(cmd.equalsIgnoreCase("something")) {
